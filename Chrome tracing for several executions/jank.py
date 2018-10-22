ead4109b17dc618a11f7fc4177e945106c1f7e96
majid3612 <majid3612@gmail.com>
2018-10-22 13:06:16 -0400

    Chrome tracing for several navigation

---
+++ Chrome tracing for several executions/jank.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import gzip
+import json
+import os
+import shutil
+
+snapshot_start='lttng snapshot record\n'
+snapshot_stop='lttng stop\nlttng start\n'
+get_time_nano='date +"%T.%9N"'
+
+def OpenTraceFile(file_path, mode):
+  if file_path.endswith('.gz'):
+    return gzip.open(file_path, mode + 'b')
+  else:
+    return open(file_path, mode + 't')
+
+def FindJank(filename):
+  with OpenTraceFile(filename, 'r') as f:
+    data = json.loads(f.read().decode('utf-8'))
+    # Find the browser main thread
+    browser_main_thread = -1
+    for event in data['traceEvents']:
+      if event['ph'] == 'M':
+        if (event['name'] == 'thread_name'
+        and 'name' in event['args']
+        and event['args']['name'] == "CrBrowserMain"):
+          browser_main_thread = event['tid']
+    if browser_main_thread == -1:
+      return
+
+   # Find jank on the main thread
+    for event in data['traceEvents']:
+      if 'tid' in event and event['tid'] == browser_main_thread and 'dur' in event and event['dur'] > 100000:
+        os.system(snapshot_start)
+	os.system(get_time_nano)
+        print "%s  ==  Event '%s' last '%d'" % (filename, event["name"], event["dur"])
+        os.system(snapshot_stop)
+  shutil.move(filename,'./bk')	    
+while True:
+    #if len(os.listdir('./out')) != 0:
+    for root, dirs, files in os.walk("out"):  
+      for filename in files:
+	#print os.path.abspath(filename)
+        FindJank(os.path.join(root, filename))
+        #if (filename != prev_filename) or (counter == 0):
+            #prev_filename = filename
+            #os.system(move_traces)
+	    #counter = 1
+            #if counter > 4:
+            #print "Cutting this file to bk2222: ", filename	    
+	    #os.system(move_traces)
+
